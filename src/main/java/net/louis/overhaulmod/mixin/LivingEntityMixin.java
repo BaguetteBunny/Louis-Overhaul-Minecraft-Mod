4e3468f9c5da05ff612aad05ea476dc90bb7ab69
Louis Billaut <ragedfear4875@gmail.com>
2025-07-14 23:16:56 +0800

    endermite teleport on hit

---
+++ src/main/java/net/louis/overhaulmod/mixin/LivingEntityMixin.java
@@ -0,0 +1,29 @@
+package net.louis.overhaulmod.mixin;
+
+import net.louis.overhaulmod.utils.TeleportUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.EndermiteEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        Entity attacker = source.getAttacker();
+        Entity target = (Entity) (Object) this;
+
+        if (attacker instanceof EndermiteEntity && target instanceof ServerPlayerEntity player) {
+            Random random = new Random();
+            if (random.nextBoolean()) {TeleportUtils.chorusTeleport(player);}
+        }
+    }
+}
+
