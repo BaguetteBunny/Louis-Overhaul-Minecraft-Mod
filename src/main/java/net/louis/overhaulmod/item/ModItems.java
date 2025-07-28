f69c1e40ab19e1f170d41ad72c2430f9d1b42d67
Louis Billaut <ragedfear4875@gmail.com>
2025-07-28 23:34:06 +0800

    add mod sound registration

--- src/main/java/net/louis/overhaulmod/item/ModItems.java
+++ src/main/java/net/louis/overhaulmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.louis.overhaulmod.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.louis.overhaulmod.LouisOverhaulMod;
+import net.louis.overhaulmod.sound.ModSounds;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.item.*;
@@ -10,13 +11,18 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-
     public static final Item BAT_FANG = registerItem("bat_fang", new Item(new Item.Settings()));
+
     public static final Item ENDERMITE_HEART = registerItem("endermite_heart", new Item(new Item.Settings()));
+
     public static final Item NETHERITE_HORSE_ARMOR = registerItem("netherite_horse_armor",
             new AnimalArmorItem(ArmorMaterials.NETHERITE, AnimalArmorItem.Type.EQUESTRIAN, false, new Item.Settings().maxCount(1)));
+
     public static final Item POTION_POUCH = registerItem("potion_pouch",
             new PotionPouchItem(new Item.Settings().maxCount(1).component(DataComponentTypes.BUNDLE_CONTENTS, BundleContentsComponent.DEFAULT)));
+
+    public static final Item SADDLED_GOAT_HORN = registerItem("saddled_goat_horn",
+            new SaddledGoatHorn(new Item.Settings().maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(LouisOverhaulMod.MOD_ID, name), item);
@@ -35,9 +41,8 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
             entries.add(POTION_POUCH);
-        });
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
             entries.add(Items.BUNDLE);
+            entries.add(SADDLED_GOAT_HORN);
         });
     }
 }
