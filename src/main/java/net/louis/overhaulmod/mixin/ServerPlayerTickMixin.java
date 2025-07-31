f3ace76031726cff99073c914df4670a6ea6c9ce
Louis Billaut <ragedfear4875@gmail.com>
2025-07-31 23:53:29 +0800

    add recall clock delay

---
+++ src/main/java/net/louis/overhaulmod/mixin/ServerPlayerTickMixin.java
@@ -0,0 +1,19 @@
+package net.louis.overhaulmod.mixin;
+
+import net.louis.overhaulmod.item.ModItems;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public class ServerPlayerTickMixin {
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void damage(CallbackInfo ci) {
+        ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
+        if (player.getHealth() < player.getMaxHealth()) {
+            player.getItemCooldownManager().set(ModItems.RECALL_CLOCK, 100);
+        }
+    }
+}
