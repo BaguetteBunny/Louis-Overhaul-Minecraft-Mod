abeb8c4418b645a60f784d43bb6e8326479b5bc2
Louis Billaut <ragedfear4875@gmail.com>
2025-08-02 23:39:06 +0800

    make warden drop reinforced deepslate

--- src/main/java/net/louis/overhaulmod/utils/ModLootTableModifiers.java
+++ src/main/java/net/louis/overhaulmod/utils/ModLootTableModifiers.java
@@ -2,6 +2,8 @@ package net.louis.overhaulmod.utils;
 
 import net.louis.overhaulmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
@@ -17,6 +19,10 @@ public class ModLootTableModifiers {
             = Identifier.of("minecraft", "entities/endermite");
     private static final Identifier HUSK_ID
             = Identifier.of("minecraft", "entities/husk");
+    private static final Identifier STRAY_ID
+            = Identifier.of("minecraft", "entities/stray");
+    private static final Identifier WARDEN_ID
+            = Identifier.of("minecraft", "entities/warden");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
@@ -46,6 +52,16 @@ public class ModLootTableModifiers {
                         .conditionally(RandomChanceLootCondition.builder(0.25f)) // 25% Drop Rate
                         .with(ItemEntry.builder(ModItems.DECAYING_FLESH))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+            // Make wardens drop reinforced deepslate
+            if(WARDEN_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // 100% Drop Rate
+                        .with(ItemEntry.builder(Items.REINFORCED_DEEPSLATE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3.0f, 8.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
             }
         });
