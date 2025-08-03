fcc323e5803f3c112716f3653ff0d4b99f95452c
Louis Billaut <ragedfear4875@gmail.com>
2025-08-03 17:41:47 +0800

    implement proper husk and stray loot table

--- src/main/java/net/louis/overhaulmod/utils/ModLootTableModifiers.java
+++ src/main/java/net/louis/overhaulmod/utils/ModLootTableModifiers.java
@@ -9,21 +9,26 @@ import net.louis.overhaulmod.LouisOverhaulMod;
 import net.louis.overhaulmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentLevelBasedValue;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.enchantment.effect.EnchantmentEffectEntry;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.EntityPropertiesLootCondition;
+import net.minecraft.loot.condition.KilledByPlayerLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.condition.RandomChanceWithEnchantedBonusLootCondition;
 import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.EnchantRandomlyLootFunction;
 import net.minecraft.loot.function.EnchantedCountIncreaseLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.function.SetPotionLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.potion.Potions;
 import net.minecraft.predicate.NbtPredicate;
 import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.registry.Registries;
@@ -73,14 +78,77 @@ public class ModLootTableModifiers {
             }
 
             if (HUSK_ID.equals(key.getValue())) {
-                LootPool.Builder customPool = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1f)) // 100% Drop Rate
-                        .with(ItemEntry.builder(ModItems.DECAYING_FLESH))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
-                        .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-                return LootTable.builder().pool(customPool).build();
+                LootTable.Builder table = LootTable.builder()
+                        // Decaying Flesh
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(RandomChanceLootCondition.builder(1f))
+                                .with(ItemEntry.builder(ModItems.DECAYING_FLESH))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()))
+
+                        // Gold Ingot
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(KilledByPlayerLootCondition.builder())
+                                .conditionally(RandomChanceWithEnchantedBonusLootCondition.builder(registry, 0.008333f,0.003333f))
+                                .with(ItemEntry.builder(Items.GOLD_INGOT))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()))
+
+                        // Beetroot
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(KilledByPlayerLootCondition.builder())
+                                .conditionally(RandomChanceWithEnchantedBonusLootCondition.builder(registry, 0.008333f,0.003333f))
+                                .with(ItemEntry.builder(Items.BEETROOT))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()))
+
+                        // Cactus
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(KilledByPlayerLootCondition.builder())
+                                .conditionally(RandomChanceWithEnchantedBonusLootCondition.builder(registry, 0.008333f,0.003333f))
+                                .with(ItemEntry.builder(Items.CACTUS))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()));
+                return table.build();
             }
+
+            if (STRAY_ID.equals(key.getValue())) {
+                LootTable.Builder table = LootTable.builder()
+                        // Chilled Bone
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(RandomChanceLootCondition.builder(1f))
+                                .with(ItemEntry.builder(ModItems.CHILLED_BONE))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()))
+
+                        // Arrow
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(RandomChanceLootCondition.builder(1f))
+                                .with(ItemEntry.builder(Items.ARROW))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()))
+
+                        // Slowness Arrow
+                        .pool(LootPool.builder()
+                                .rolls(ConstantLootNumberProvider.create(1))
+                                .conditionally(KilledByPlayerLootCondition.builder())
+                                .conditionally(RandomChanceWithEnchantedBonusLootCondition.builder(registry, 0.5f,0.15f))
+                                .with(ItemEntry.builder(Items.TIPPED_ARROW).apply(SetPotionLootFunction.builder(Potions.SLOWNESS)))
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build())
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build()));
+                return table.build();
+            }
+
+
+
+
+
             return null;
         });
     }
