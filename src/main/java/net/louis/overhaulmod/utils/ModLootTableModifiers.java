5374e099f43b8d248bd7ba8aa89a4d72c45aed02
Louis Billaut <ragedfear4875@gmail.com>
2025-08-03 17:07:48 +0800

    name and loot table changes

--- src/main/java/net/louis/overhaulmod/utils/ModLootTableModifiers.java
+++ src/main/java/net/louis/overhaulmod/utils/ModLootTableModifiers.java
@@ -1,25 +1,47 @@
 package net.louis.overhaulmod.utils;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
+import net.louis.overhaulmod.LouisOverhaulMod;
 import net.louis.overhaulmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentLevelBasedValue;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.enchantment.effect.EnchantmentEffectEntry;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.EntityPropertiesLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.condition.RandomChanceWithEnchantedBonusLootCondition;
+import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.ApplyBonusLootFunction;
+import net.minecraft.loot.entry.LootPoolEntry;
+import net.minecraft.loot.function.EnchantRandomlyLootFunction;
+import net.minecraft.loot.function.EnchantedCountIncreaseLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.NbtPredicate;
+import net.minecraft.predicate.entity.EntityPredicate;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.louis.overhaulmod.LouisOverhaulMod.LOGGER;
 
 public class ModLootTableModifiers {
     private static final Identifier BAT_ID
@@ -35,12 +57,28 @@ public class ModLootTableModifiers {
 
     public static void replaceLootTables() {
         LootTableEvents.REPLACE.register((key, lootManager, source, registry) -> {
-            if(WARDEN_ID.equals(key.getValue())) {
+            RegistryKey<Enchantment> lootingKey = Enchantments.LOOTING;
+            RegistryWrapper<Enchantment> enchantmentRegistry = registry.getWrapperOrThrow(RegistryKeys.ENCHANTMENT);
+            RegistryEntry<Enchantment> lootingEntry = enchantmentRegistry.getOrThrow(lootingKey);
+            Enchantment lootingEnchant = lootingEntry.value();
+            System.out.println(lootingEnchant);
+
+            if (WARDEN_ID.equals(key.getValue())) {
                 LootPool.Builder customPool = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(1f)) // 100% Drop Rate
                         .with(ItemEntry.builder(Items.REINFORCED_DEEPSLATE))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3.0f, 8.0f)).build());
+                return LootTable.builder().pool(customPool).build();
+            }
+
+            if (HUSK_ID.equals(key.getValue())) {
+                LootPool.Builder customPool = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f)) // 100% Drop Rate
+                        .with(ItemEntry.builder(ModItems.DECAYING_FLESH))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
+                        .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 return LootTable.builder().pool(customPool).build();
             }
             return null;
@@ -67,6 +105,7 @@ public class ModLootTableModifiers {
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
                 tableBuilder.pool(poolBuilder.build());
             }
+
         });
     }
 }
