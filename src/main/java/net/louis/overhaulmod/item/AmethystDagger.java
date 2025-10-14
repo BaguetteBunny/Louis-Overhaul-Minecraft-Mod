380ba437f02625bbe7fb0f3fde147e06cfaa9e92
Louis Billaut <ragedfear4875@gmail.com>
2025-10-14 20:53:57 +0800

    add wood stripping

--- src/main/java/net/louis/overhaulmod/item/AmethystDagger.java
+++ src/main/java/net/louis/overhaulmod/item/AmethystDagger.java
@@ -32,7 +32,18 @@ public class AmethystDagger extends PickaxeItem {
             Map.entry(Blocks.MANGROVE_LOG, Blocks.STRIPPED_MANGROVE_LOG),
             Map.entry(Blocks.CHERRY_LOG, Blocks.STRIPPED_CHERRY_LOG),
             Map.entry(Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM),
-            Map.entry(Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM)
+            Map.entry(Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM),
+
+            Map.entry(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD),
+            Map.entry(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD),
+            Map.entry(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD),
+            Map.entry(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD),
+            Map.entry(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD),
+            Map.entry(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD),
+            Map.entry(Blocks.MANGROVE_WOOD, Blocks.STRIPPED_MANGROVE_WOOD),
+            Map.entry(Blocks.CHERRY_WOOD, Blocks.STRIPPED_CHERRY_WOOD),
+            Map.entry(Blocks.CRIMSON_HYPHAE, Blocks.STRIPPED_CRIMSON_HYPHAE),
+            Map.entry(Blocks.WARPED_HYPHAE, Blocks.STRIPPED_WARPED_HYPHAE)
     );
 
     @Override
