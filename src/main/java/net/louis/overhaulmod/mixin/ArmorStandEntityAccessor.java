52eb205ad5a76312d66d043df053c757ed1fb609
Louis Billaut <ragedfear4875@gmail.com>
2025-10-18 20:31:12 +0800

    why is onbreak private bruh

--- src/main/java/net/louis/overhaulmod/mixin/ArmorStandEntityAccessor.java
+++ src/main/java/net/louis/overhaulmod/mixin/ArmorStandEntityAccessor.java
@@ -1,6 +1,8 @@
 package net.louis.overhaulmod.mixin;
 
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
@@ -8,4 +10,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 public interface ArmorStandEntityAccessor {
     @Invoker("setSmall")
     void callSetSmall(boolean small);
+
+    @Invoker("onBreak")
+    void callonBreak(ServerWorld world, DamageSource damageSource);
 }
