169e390dadeb028d93fc63861734b43590c75f88
Louis Billaut <ragedfear4875@gmail.com>
2025-10-18 20:31:52 +0800

    fix phantom dupe glitch

--- src/main/java/net/louis/overhaulmod/mixin/ItemFrameMixin.java
+++ src/main/java/net/louis/overhaulmod/mixin/ItemFrameMixin.java
@@ -1,15 +1,22 @@
 package net.louis.overhaulmod.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemFrameEntity.class)
 public class ItemFrameMixin {
@@ -24,5 +31,23 @@ public class ItemFrameMixin {
             ItemEntity itemEntity = new ItemEntity(world, frame.getX(), frame.getY() + 0.1, frame.getZ(), stack);
             world.spawnEntity(itemEntity);
         }
+    }
+
+    @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
+    private void onInteract(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        ItemStack stack = player.getStackInHand(hand);
+        ItemFrameEntity frame = ((ItemFrameEntity) (Object) this);
+
+        if (!player.isSneaking() || !stack.isOf(Items.PHANTOM_MEMBRANE) || frame.isInvisible()) {
+            return;
+        }
+
+        frame.setInvisible(true);
+        stack.damage(1, player, EquipmentSlot.MAINHAND);
+        frame.getWorld().playSound(null, frame.getBlockPos(),
+                SoundEvents.BLOCK_ENCHANTMENT_TABLE_USE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+
+        cir.setReturnValue(ActionResult.SUCCESS);
+        cir.cancel();
     }
 }
