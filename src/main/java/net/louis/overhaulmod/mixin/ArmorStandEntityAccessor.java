945c94b083e2711779a64a91b1de1e7ce38d3d18
Louis Billaut <ragedfear4875@gmail.com>
2025-10-18 18:30:28 +0800

    why the fuck is setsmall private

---
+++ src/main/java/net/louis/overhaulmod/mixin/ArmorStandEntityAccessor.java
@@ -0,0 +1,11 @@
+package net.louis.overhaulmod.mixin;
+
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ArmorStandEntity.class)
+public interface ArmorStandEntityAccessor {
+    @Invoker("setSmall")
+    void callSetSmall(boolean small);
+}
