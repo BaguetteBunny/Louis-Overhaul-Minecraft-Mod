f66f796ba865a47844d087c8e53afd5ebef269c6
Louis Billaut <ragedfear4875@gmail.com>
2025-10-25 20:10:48 +0800

    nerf grounded by making it not activate for certain conditions

--- src/main/java/net/louis/overhaulmod/effect/GroundedEffect.java
+++ src/main/java/net/louis/overhaulmod/effect/GroundedEffect.java
@@ -3,6 +3,7 @@ package net.louis.overhaulmod.effect;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class GroundedEffect extends StatusEffect {
     public GroundedEffect(StatusEffectCategory category, int color){
@@ -11,8 +12,8 @@ public class GroundedEffect extends StatusEffect {
 
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
-        if (!entity.isOnGround()) {
-            entity.addVelocity(0, -0.1, 0);
+        if (!entity.isOnGround() && !(entity instanceof PlayerEntity player && disallowedPlayerStates(player))) {
+            entity.addVelocity(0, -0.025, 0);
         }
 
         return super.applyUpdateEffect(entity, amplifier);
@@ -21,5 +22,14 @@ public class GroundedEffect extends StatusEffect {
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
+    }
+
+    private boolean disallowedPlayerStates(PlayerEntity player) {
+        return player.isSpectator()
+                || player.isSwimming()
+                || player.isPushedByFluids()
+                || player.isFallFlying()
+                || player.isUsingRiptide()
+                || player.getAbilities().allowFlying;
     }
 }
