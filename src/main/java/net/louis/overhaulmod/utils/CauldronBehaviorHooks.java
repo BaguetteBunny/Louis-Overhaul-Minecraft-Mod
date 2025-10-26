eed7b435b9d69f552b3f5097c63e9ca5626d19db
Louis Billaut <ragedfear4875@gmail.com>
2025-10-27 03:10:40 +0800

    make cauldrons wash everything

---
+++ src/main/java/net/louis/overhaulmod/utils/CauldronBehaviorHooks.java
@@ -0,0 +1,77 @@
+package net.louis.overhaulmod.utils;
+
+import net.minecraft.block.LeveledCauldronBlock;
+import net.minecraft.block.cauldron.CauldronBehavior;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ItemActionResult;
+
+public class CauldronBehaviorHooks {
+    public static CauldronBehavior CLEAN_BLOCK = (state, world, pos, player, hand, stack) -> {
+        if (!world.isClient) {
+            Item i;
+            if (stack.isIn(ItemTags.WOOL)) {i = Items.WHITE_WOOL;}
+            else if (stack.isIn(ItemTags.CANDLES)) {i = Items.CANDLE;}
+            else if (stack.isIn(ItemTags.WOOL_CARPETS)) {i = Items.WHITE_CARPET;}
+            else if (stack.isIn(ItemTags.TERRACOTTA)) {i = Items.TERRACOTTA;}
+            else if (stack.isIn(ItemTags.WOOL)) {i = Items.WHITE_WOOL;}
+            else if (stack.isIn(ItemTags.BEDS)) {i = Items.WHITE_BED;}
+            else {return ItemActionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;}
+
+            int count = player.isSneaking() ? stack.getCount() : 1;
+
+            stack.decrement(count);
+            if (!player.getInventory().insertStack(new ItemStack(i, count))) player.dropItem(stack, true);
+            player.incrementStat(Stats.USE_CAULDRON);
+            LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
+        }
+        return ItemActionResult.success(world.isClient);
+    };
+
+    public static CauldronBehavior CLEAN_CONCRETE = (state, world, pos, player, hand, stack) -> {
+        if (!world.isClient) {
+            int count = stack.getCount();
+            stack.decrement(count);
+            if (!player.getInventory().insertStack(new ItemStack(Items.WHITE_CONCRETE, count))) player.dropItem(stack, true);
+            player.incrementStat(Stats.USE_CAULDRON);
+            LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
+        }
+        return ItemActionResult.success(world.isClient);
+    };
+
+    public static CauldronBehavior CLEAN_CONCRETE_POWDER = (state, world, pos, player, hand, stack) -> {
+        if (!world.isClient) {
+            int count = player.isSneaking() ? stack.getCount() : 1;
+            stack.decrement(count);
+            if (!player.getInventory().insertStack(new ItemStack(Items.WHITE_CONCRETE_POWDER, count))) player.dropItem(stack, true);
+            player.incrementStat(Stats.USE_CAULDRON);
+            LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
+        }
+        return ItemActionResult.success(world.isClient);
+    };
+
+    public static CauldronBehavior CLEAN_GLASS = (state, world, pos, player, hand, stack) -> {
+        if (!world.isClient) {
+            int count = player.isSneaking() ? stack.getCount() : 1;
+            stack.decrement(count);
+            if (!player.getInventory().insertStack(new ItemStack(Items.GLASS, count))) player.dropItem(stack, true);
+            player.incrementStat(Stats.USE_CAULDRON);
+            LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
+        }
+        return ItemActionResult.success(world.isClient);
+    };
+
+    public static CauldronBehavior CLEAN_GLASS_PANE = (state, world, pos, player, hand, stack) -> {
+        if (!world.isClient) {
+            int count = player.isSneaking() ? stack.getCount() : 1;
+            stack.decrement(count);
+            if (!player.getInventory().insertStack(new ItemStack(Items.GLASS_PANE, count))) player.dropItem(stack, true);
+            player.incrementStat(Stats.USE_CAULDRON);
+            LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
+        }
+        return ItemActionResult.success(world.isClient);
+    };
+}
