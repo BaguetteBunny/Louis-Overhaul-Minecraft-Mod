2c89948bdc6cb77114ebecb1f7af486af089b954
Louis Billaut <ragedfear4875@gmail.com>
2025-11-01 13:50:33 +0800

    make villagers use advanced fletching table

--- src/main/java/net/louis/overhaulmod/LouisOverhaulMod.java
+++ src/main/java/net/louis/overhaulmod/LouisOverhaulMod.java
@@ -1,7 +1,9 @@
 package net.louis.overhaulmod;
 
+import com.google.common.collect.ImmutableSet;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
 import net.louis.overhaulmod.block.ModBlocks;
 import net.louis.overhaulmod.block.entity.ModBlockEntities;
 import net.louis.overhaulmod.component.ModComponents;
@@ -14,8 +16,18 @@ import net.louis.overhaulmod.sound.ModSounds;
 import net.louis.overhaulmod.utils.DespawnManager;
 import net.louis.overhaulmod.utils.GlowManager;
 import net.louis.overhaulmod.utils.ModLootTableModifiers;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.poi.PointOfInterestType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
 
 public class LouisOverhaulMod implements ModInitializer {
 	public static final String MOD_ID = "louis-overhaul-mod";
@@ -40,6 +52,7 @@ public class LouisOverhaulMod implements ModInitializer {
 		ModUseEvents.registerProjectileItems();
 
 		tickGlobal();
+		replaceFletcherPOI();
 	}
 
 	private void tickGlobal() {
@@ -47,5 +60,10 @@ public class LouisOverhaulMod implements ModInitializer {
 			GlowManager.tick();
 			DespawnManager.tick();
 		});
+	}
+
+	private void replaceFletcherPOI() {
+		Set<BlockState> states = ImmutableSet.copyOf(ModBlocks.ADVANCED_FLETCHING_TABLE.getStateManager().getStates());
+		PointOfInterestHelper.register(Identifier.ofVanilla("fletcher"), 1, 1, states);
 	}
 }
