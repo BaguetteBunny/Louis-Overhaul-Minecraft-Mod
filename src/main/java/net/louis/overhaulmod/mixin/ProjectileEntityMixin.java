7428c363f843f1831268928d209e8bf3e41fb5d2
Louis Billaut <ragedfear4875@gmail.com>
2025-11-01 17:53:35 +0800

    make armadillo scute fletching disable iframes

--- src/main/java/net/louis/overhaulmod/mixin/ProjectileEntityMixin.java
+++ src/main/java/net/louis/overhaulmod/mixin/ProjectileEntityMixin.java
@@ -4,6 +4,7 @@ import net.louis.overhaulmod.component.ModComponents;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.component.ComponentMap;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
@@ -11,6 +12,7 @@ import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -43,6 +45,21 @@ public class ProjectileEntityMixin {
                 world.playSound(null, pos, SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.BLOCKS, 3.0F, 0.5F);
                 ci.cancel();
             }
+        }
+    }
+
+    @Inject(method = "onCollision", at = @At("TAIl"), cancellable = true)
+    protected void disableEntityIFrames(HitResult hitResult, CallbackInfo ci) {
+        World world = self.getWorld();
+        if (
+                !world.isClient && self instanceof ArrowEntity arrow &&
+                        hitResult.getType() == HitResult.Type.ENTITY &&
+                        arrow.getItemStack().getComponents().contains(ModComponents.ARROW_FOOT) &&
+                        Items.ARMADILLO_SCUTE.equals(arrow.getItemStack().getComponents().get(ModComponents.ARROW_FOOT))
+        ) {
+            EntityHitResult entityHitResult = (EntityHitResult)hitResult;
+            Entity entity = entityHitResult.getEntity();
+            entity.timeUntilRegen = 0;
         }
     }
 }
