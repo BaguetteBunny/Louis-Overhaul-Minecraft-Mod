838a8a7a1893f27f363fd5ce6791e78474e73f8b
Louis Billaut <ragedfear4875@gmail.com>
2025-11-09 21:40:43 +0800

    made mob heads more reliable

--- src/main/java/net/louis/overhaulmod/mixin/LivingEntityMixin.java
+++ src/main/java/net/louis/overhaulmod/mixin/LivingEntityMixin.java
@@ -2,14 +2,21 @@ package net.louis.overhaulmod.mixin;
 
 import net.louis.overhaulmod.effect.ModEffects;
 import net.louis.overhaulmod.utils.TeleportUtils;
+import net.minecraft.block.SkullBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.EndermiteEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -33,6 +40,31 @@ public class LivingEntityMixin {
         LivingEntity self = (LivingEntity) (Object) this;
         if (self.hasStatusEffect(ModEffects.GROUNDED)) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "getAttackDistanceScalingFactor", at = @At("RETURN"), cancellable = true)
+    private void injectedHeadDetectionReduction(Entity entity, CallbackInfoReturnable<Double> cir) {
+        LivingEntity self = (LivingEntity) (Object) this;
+        double original = cir.getReturnValue();
+        ItemStack head = self.getEquippedStack(EquipmentSlot.HEAD);
+
+        if (entity != null) {
+            if (entity.getType().equals(EntityType.SKELETON) && head.isOf(Items.SKELETON_SKULL)
+                    || entity.getType().equals(EntityType.STRAY) && head.isOf(Items.SKELETON_SKULL)
+                    || entity.getType().equals(EntityType.BOGGED) && head.isOf(Items.SKELETON_SKULL)
+                    || entity.getType().equals(EntityType.ZOMBIE) && head.isOf(Items.ZOMBIE_HEAD)
+                    || entity.getType().equals(EntityType.HUSK) && head.isOf(Items.ZOMBIE_HEAD)
+                    || entity.getType().equals(EntityType.DROWNED) && head.isOf(Items.ZOMBIE_HEAD)
+                    || entity.getType().equals(EntityType.ZOMBIE_VILLAGER) && head.isOf(Items.ZOMBIE_HEAD)
+                    || entity.getType().equals(EntityType.PIGLIN) && head.isOf(Items.PIGLIN_HEAD)
+                    || entity.getType().equals(EntityType.PIGLIN_BRUTE) && head.isOf(Items.PIGLIN_HEAD)
+                    || entity.getType().equals(EntityType.ZOMBIFIED_PIGLIN) && head.isOf(Items.PIGLIN_HEAD)
+                    || entity.getType().equals(EntityType.CREEPER) && head.isOf(Items.CREEPER_HEAD)) {
+                cir.setReturnValue(.0);
+            } else {
+                cir.setReturnValue(original);
+            }
         }
     }
 }
