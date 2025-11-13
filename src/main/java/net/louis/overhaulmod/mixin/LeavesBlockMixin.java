35f6f2ae481f032174f9cb6e7f738cc13ce34c60
Louis Billaut <ragedfear4875@gmail.com>
2025-11-14 03:07:02 +0800

    make jungle saplings more common

---
+++ src/main/java/net/louis/overhaulmod/mixin/LeavesBlockMixin.java
@@ -0,0 +1,26 @@
+package net.louis.overhaulmod.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LeavesBlock.class)
+public class LeavesBlockMixin {
+
+    private void onRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (!(Boolean)state.get(LeavesBlock.PERSISTENT) && (Integer)state.get(LeavesBlock.DISTANCE) == 7) {
+            LeavesBlock.dropStacks(state, world, pos);
+            if (state.isOf(Blocks.JUNGLE_LEAVES) && random.nextInt(35) == 0) {
+                LeavesBlock.dropStack(world, pos, new ItemStack(Blocks.JUNGLE_SAPLING));
+            }
+            world.removeBlock(pos, false);
+            ci.cancel();
+        }
+    }
+}
