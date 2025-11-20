2d2d907b53f1d22559eabbb0ea693f10391d04de
Louis Billaut <ragedfear4875@gmail.com>
2025-11-20 20:05:56 +0800

    smelting enchant recipe hotfix

--- src/main/java/net/louis/overhaulmod/enchantments/custom/SmeltingEnchantmentEffect.java
+++ src/main/java/net/louis/overhaulmod/enchantments/custom/SmeltingEnchantmentEffect.java
@@ -26,15 +26,15 @@ public record SmeltingEnchantmentEffect() implements EnchantmentEntityEffect {
         TRANSFORM_MAP.put(Blocks.DEEPSLATE_IRON_ORE, Items.IRON_INGOT);
         TRANSFORM_MAP.put(Blocks.DEEPSLATE_GOLD_ORE, Items.GOLD_INGOT);
         TRANSFORM_MAP.put(Blocks.DEEPSLATE_COPPER_ORE, Items.COPPER_INGOT);
-        TRANSFORM_MAP.put(Blocks.RAW_IRON_BLOCK, Items.IRON_INGOT);
-        TRANSFORM_MAP.put(Blocks.RAW_GOLD_BLOCK, Items.GOLD_INGOT);
-        TRANSFORM_MAP.put(Blocks.RAW_COPPER_BLOCK, Items.COPPER_INGOT);
+        TRANSFORM_MAP.put(Blocks.RAW_IRON_BLOCK, Items.IRON_BLOCK);
+        TRANSFORM_MAP.put(Blocks.RAW_GOLD_BLOCK, Items.GOLD_BLOCK);
+        TRANSFORM_MAP.put(Blocks.RAW_COPPER_BLOCK, Items.COPPER_BLOCK);
         TRANSFORM_MAP.put(Blocks.ANCIENT_DEBRIS, Items.NETHERITE_SCRAP);
         TRANSFORM_MAP.put(Blocks.SEA_PICKLE, Items.LIME_DYE);
         TRANSFORM_MAP.put(Blocks.CACTUS, Items.GREEN_DYE);
 
         TRANSFORM_MAP.put(Blocks.CLAY, Items.TERRACOTTA);
-        TRANSFORM_MAP.put(Blocks.SPONGE, Items.WET_SPONGE);
+        TRANSFORM_MAP.put(Blocks.WET_SPONGE, Items.SPONGE);
         TRANSFORM_MAP.put(Blocks.NETHERRACK, Items.NETHER_BRICKS);
 
         TRANSFORM_MAP.put(Blocks.POTATOES, Items.BAKED_POTATO);
