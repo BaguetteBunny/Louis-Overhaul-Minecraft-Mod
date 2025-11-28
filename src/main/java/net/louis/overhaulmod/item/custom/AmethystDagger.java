e45085042490063397165b428144cbac38d75e43
Louis Billaut <ragedfear4875@gmail.com>
2025-11-28 16:56:58 +0800

    modified amethyst dagger

--- src/main/java/net/louis/overhaulmod/item/custom/AmethystDagger.java
+++ src/main/java/net/louis/overhaulmod/item/custom/AmethystDagger.java
@@ -5,9 +5,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.PickaxeItem;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
@@ -15,6 +18,7 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 import java.util.Map;
+import java.util.Set;
 
 public class AmethystDagger extends PickaxeItem {
     public AmethystDagger(Settings settings) {
@@ -23,121 +27,121 @@ public class AmethystDagger extends PickaxeItem {
         );
     }
 
-    private static final Map<Block, Block> STRIPPABLES = Map.ofEntries(
-            Map.entry(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG),
-            Map.entry(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG),
-            Map.entry(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG),
-            Map.entry(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG),
-            Map.entry(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG),
-            Map.entry(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG),
-            Map.entry(Blocks.MANGROVE_LOG, Blocks.STRIPPED_MANGROVE_LOG),
-            Map.entry(Blocks.CHERRY_LOG, Blocks.STRIPPED_CHERRY_LOG),
-            Map.entry(Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM),
-            Map.entry(Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM),
-
-            Map.entry(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD),
-            Map.entry(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD),
-            Map.entry(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD),
-            Map.entry(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD),
-            Map.entry(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD),
-            Map.entry(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD),
-            Map.entry(Blocks.MANGROVE_WOOD, Blocks.STRIPPED_MANGROVE_WOOD),
-            Map.entry(Blocks.CHERRY_WOOD, Blocks.STRIPPED_CHERRY_WOOD),
-            Map.entry(Blocks.CRIMSON_HYPHAE, Blocks.STRIPPED_CRIMSON_HYPHAE),
-            Map.entry(Blocks.WARPED_HYPHAE, Blocks.STRIPPED_WARPED_HYPHAE)
-    );
-
     private static final Map<Block, Block> DEFROSTABLES = Map.ofEntries(
             Map.entry(Blocks.BLUE_ICE, Blocks.PACKED_ICE),
             Map.entry(Blocks.PACKED_ICE, Blocks.ICE)
     );
 
-    private static final Map<Block, Block> SAPPLINGABLES = Map.ofEntries(
-            Map.entry(Blocks.OAK_LEAVES, Blocks.OAK_SAPLING),
-            Map.entry(Blocks.BIRCH_LEAVES, Blocks.BIRCH_SAPLING),
-            Map.entry(Blocks.SPRUCE_LEAVES, Blocks.SPRUCE_SAPLING),
-            Map.entry(Blocks.JUNGLE_LEAVES, Blocks.JUNGLE_SAPLING),
-            Map.entry(Blocks.ACACIA_LEAVES, Blocks.ACACIA_SAPLING),
-            Map.entry(Blocks.DARK_OAK_LEAVES, Blocks.DARK_OAK_SAPLING),
-            Map.entry(Blocks.MANGROVE_LEAVES, Blocks.MANGROVE_PROPAGULE),
-            Map.entry(Blocks.CHERRY_LEAVES, Blocks.CHERRY_SAPLING),
-            Map.entry(Blocks.AZALEA_LEAVES, Blocks.AZALEA),
-            Map.entry(Blocks.FLOWERING_AZALEA_LEAVES, Blocks.FLOWERING_AZALEA)
+    private static final Set<Item> REDSTONES = Set.of(
+            Items.REPEATER,
+            Items.REDSTONE_TORCH,
+            Items.REDSTONE_LAMP,
+            Items.COMPARATOR
+    );
+
+    public static final Set<Block> UNDROPPABLES = Set.of(
+            Blocks.GLASS,
+            Blocks.BLACK_STAINED_GLASS,
+            Blocks.BLUE_STAINED_GLASS,
+            Blocks.BROWN_STAINED_GLASS,
+            Blocks.CYAN_STAINED_GLASS,
+            Blocks.GRAY_STAINED_GLASS,
+            Blocks.GREEN_STAINED_GLASS,
+            Blocks.LIGHT_BLUE_STAINED_GLASS,
+            Blocks.LIGHT_GRAY_STAINED_GLASS,
+            Blocks.LIME_STAINED_GLASS,
+            Blocks.MAGENTA_STAINED_GLASS,
+            Blocks.ORANGE_STAINED_GLASS,
+            Blocks.PINK_STAINED_GLASS,
+            Blocks.PURPLE_STAINED_GLASS,
+            Blocks.RED_STAINED_GLASS,
+            Blocks.WHITE_STAINED_GLASS,
+            Blocks.YELLOW_STAINED_GLASS,
+            Blocks.GLASS_PANE,
+            Blocks.BLACK_STAINED_GLASS_PANE,
+            Blocks.BLUE_STAINED_GLASS_PANE,
+            Blocks.BROWN_STAINED_GLASS_PANE,
+            Blocks.CYAN_STAINED_GLASS_PANE,
+            Blocks.GRAY_STAINED_GLASS_PANE,
+            Blocks.GREEN_STAINED_GLASS_PANE,
+            Blocks.LIGHT_BLUE_STAINED_GLASS_PANE,
+            Blocks.LIGHT_GRAY_STAINED_GLASS_PANE,
+            Blocks.LIME_STAINED_GLASS_PANE,
+            Blocks.MAGENTA_STAINED_GLASS_PANE,
+            Blocks.ORANGE_STAINED_GLASS_PANE,
+            Blocks.PINK_STAINED_GLASS_PANE,
+            Blocks.PURPLE_STAINED_GLASS_PANE,
+            Blocks.RED_STAINED_GLASS_PANE,
+            Blocks.WHITE_STAINED_GLASS_PANE,
+            Blocks.YELLOW_STAINED_GLASS_PANE
+    );
+
+    public static final Set<Block> DROPPABLES = Set.of(
+            Blocks.SKELETON_SKULL,
+            Blocks.SKELETON_WALL_SKULL,
+            Blocks.WITHER_SKELETON_SKULL,
+            Blocks.WITHER_SKELETON_WALL_SKULL,
+            Blocks.ZOMBIE_HEAD,
+            Blocks.ZOMBIE_WALL_HEAD,
+            Blocks.CREEPER_HEAD,
+            Blocks.CREEPER_WALL_HEAD,
+            Blocks.DRAGON_HEAD,
+            Blocks.DRAGON_WALL_HEAD,
+            Blocks.PIGLIN_HEAD,
+            Blocks.PIGLIN_WALL_HEAD,
+            Blocks.PLAYER_HEAD,
+            Blocks.PLAYER_WALL_HEAD,
+
+            Blocks.HEAVY_CORE,
+            Blocks.TINTED_GLASS
     );
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack daggerStack = user.getStackInHand(hand);
+        ItemStack daggerStack = user.getMainHandStack();
         ItemStack offHand = user.getOffHandStack();
 
-        if (hand == Hand.MAIN_HAND && !offHand.isEmpty() && world.isClient) {
-            Block offhandBlock = Block.getBlockFromItem(offHand.getItem());
+        if (offHand.isEmpty() || world.isClient()) return TypedActionResult.pass(daggerStack);
 
-            if (STRIPPABLES.containsKey(offhandBlock)) {
-                Block newBlock = STRIPPABLES.get(offhandBlock);
-                offHand.decrement(1);
-                user.getInventory().offerOrDrop(new ItemStack(newBlock));
-                world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                        SoundEvents.ITEM_AXE_STRIP, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                daggerStack.damage(1, user, EquipmentSlot.MAINHAND);
+        boolean success = true;
+        Block offhandBlock = Block.getBlockFromItem(offHand.getItem());
 
-                return TypedActionResult.success(daggerStack, world.isClient());
-            }
-
-            if (DEFROSTABLES.containsKey(offhandBlock)) {
-                Block newBlock = DEFROSTABLES.get(offhandBlock);
-                offHand.decrement(1);
-                user.getInventory().offerOrDrop(new ItemStack(newBlock, 8));
-                world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                        SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.BLOCKS, 1.0F, 2.0F);
-                daggerStack.damage(4, user, EquipmentSlot.MAINHAND);
-
-                return TypedActionResult.success(daggerStack, world.isClient());
-            }
-
-            if (SAPPLINGABLES.containsKey(offhandBlock)) {
-                Block newBlock = SAPPLINGABLES.get(offhandBlock);
-                offHand.decrement(1);
-                if (world.random.nextFloat() < 0.02) {
-                    user.getInventory().offerOrDrop(new ItemStack(Items.APPLE, 1));
-                }
-                if (world.random.nextFloat() < 0.55) {
-                    user.getInventory().offerOrDrop(new ItemStack(newBlock, 1));
-                }
-
-                world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                        SoundEvents.BLOCK_CHERRY_LEAVES_BREAK, SoundCategory.BLOCKS, 1.0F, 2.0F);
-                daggerStack.damage(1, user, EquipmentSlot.MAINHAND);
-
-                return TypedActionResult.success(daggerStack, world.isClient());
-            }
-
-            if (offhandBlock.equals(Blocks.CRYING_OBSIDIAN)) {
-                offHand.decrement(1);
-                user.getInventory().offerOrDrop(new ItemStack(Items.OBSIDIAN, 1));
-                world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                        SoundEvents.BLOCK_CANDLE_EXTINGUISH, SoundCategory.BLOCKS, 1.0F, 2.0F);
-                daggerStack.damage(1, user, EquipmentSlot.MAINHAND);
-
-                return TypedActionResult.success(daggerStack, world.isClient());
-            }
-
-            if (offhandBlock.equals(Blocks.AMETHYST_BLOCK)) {
-                offHand.decrement(1);
-                if (world.random.nextFloat() < 0.05) {
-                    user.getInventory().offerOrDrop(new ItemStack(Blocks.BUDDING_AMETHYST, 1));
-                    world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                            SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 2.0F, 0.5F);
-                } else {
-                    world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                            SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 2.0F, 2.0F);
-                }
-                daggerStack.damage(1, user, EquipmentSlot.MAINHAND);
-
-                return TypedActionResult.success(daggerStack, world.isClient());
-            }
+        if (DEFROSTABLES.containsKey(offhandBlock)) {
+            Block newBlock = DEFROSTABLES.get(offhandBlock);
+            user.getInventory().offerOrDrop(new ItemStack(newBlock, 8));
+            world.playSound(null, user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.BLOCKS, 1.0F, 2.0F);
+            daggerStack.damage(3, user, EquipmentSlot.MAINHAND);
         }
 
-        return super.use(world, user, hand);
+        else if (REDSTONES.contains(offHand.getItem())) {
+            user.getInventory().offerOrDrop(new ItemStack(Items.REDSTONE, 1));
+            world.playSound(null, user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA, SoundCategory.BLOCKS, 1.0F, 2.0F);
+        }
+
+        else if (offHand.getItem().equals(Items.FILLED_MAP)) {
+            user.getInventory().offerOrDrop(new ItemStack(Items.MAP, 1));
+            world.playSound(null, user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA, SoundCategory.BLOCKS, 1.0F, 2.0F);
+        }
+
+        else if (offhandBlock.equals(Blocks.AMETHYST_BLOCK)) {
+            user.getInventory().offerOrDrop(new ItemStack(Blocks.AMETHYST_CLUSTER, 1));
+            world.playSound(null, user.getX(), user.getY(), user.getZ(),
+                    SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 2.0F, 0.5F);
+        }
+
+        else success = false;
+
+
+        if (success) {
+            offHand.decrement(1);
+            daggerStack.damage(1, user, EquipmentSlot.MAINHAND);
+            user.swingHand(Hand.MAIN_HAND, true);
+            return TypedActionResult.success(daggerStack, world.isClient());
+        }
+
+        return TypedActionResult.pass(daggerStack);
     }
 }
--- src/main/java/net/louis/overhaulmod/mixin/AbstractBlockMixin.java
+++ src/main/java/net/louis/overhaulmod/mixin/AbstractBlockMixin.java
@@ -1,16 +1,46 @@
 package net.louis.overhaulmod.mixin;
 
+import com.mojang.authlib.GameProfile;
+import net.louis.overhaulmod.item.ModItems;
 import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.SkullBlockEntity;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.context.LootContextParameterSet;
+import net.minecraft.loot.context.LootContextParameters;
+import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collections;
+import java.util.List;
+
+import static net.louis.overhaulmod.item.custom.AmethystDagger.DROPPABLES;
+import static net.louis.overhaulmod.item.custom.AmethystDagger.UNDROPPABLES;
 
 @Mixin(AbstractBlock.class)
 public class AbstractBlockMixin {
@@ -23,6 +53,64 @@ public class AbstractBlockMixin {
                 cir.setReturnValue(VoxelShapes.empty());
             }
         }
+    }
+
+    @Inject(method = "onBlockBreakStart", at = @At("HEAD"), cancellable = true)
+    protected void useAmethystDagger(BlockState state, World world, BlockPos pos, PlayerEntity player, CallbackInfo ci) {
+        Block bl = state.getBlock();
+        ItemStack stack = player.getMainHandStack();
+        if (!(world instanceof ServerWorld)) return;
+
+        if (stack.getItem().equals(ModItems.AMETHYST_DAGGER)) {
+            if (UNDROPPABLES.contains(bl)) {
+                stack.damage(1, player, EquipmentSlot.MAINHAND);
+                world.breakBlock(pos, false, player);
+                player.incrementStat(Stats.MINED.getOrCreateStat(bl));
+                player.addExhaustion(0.005F);
+                ci.cancel();
+            }
+
+            if (DROPPABLES.contains(bl)) {
+                stack.damage(1, player, EquipmentSlot.MAINHAND);
+                ItemStack drop = new ItemStack(bl);
+
+                if (state.getBlock() instanceof PlayerSkullBlock) {
+                    BlockEntity be = world.getBlockEntity(pos);
+                    ProfileComponent profileComponent = getProfileComponent(be);
+                    world.breakBlock(pos, false, player);
+                    if (profileComponent != null) drop.set(DataComponentTypes.PROFILE, profileComponent);
+                }
+
+                world.breakBlock(pos, false, player);
+                world.spawnEntity(new ItemEntity(
+                        world,
+                        pos.getX() + 0.5,
+                        pos.getY() + 0.5,
+                        pos.getZ() + 0.5,
+                        drop
+                ));
+
+                player.incrementStat(Stats.MINED.getOrCreateStat(bl));
+                player.addExhaustion(0.005f);
+                ci.cancel();
+            }
+        }
+    }
+
+    private static @Nullable ProfileComponent getProfileComponent(BlockEntity be) {
+        ProfileComponent profileComponent = null;
+        if (be instanceof SkullBlockEntity skull) {
+
+            ProfileComponent stored = skull.getOwner();
+            if (stored != null) {
+                profileComponent = new ProfileComponent(
+                        stored.name(),
+                        stored.id(),
+                        stored.properties()
+                );
+            }
+        }
+        return profileComponent;
     }
 }
 
